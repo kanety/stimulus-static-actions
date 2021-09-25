f6c5f38030c2b1054080cf1adcaa60862420a7fe
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2021-09-25 10:29:21 +0900

    Fix babel targets to esmodules

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     ["@babel/preset-env", {
-      "targets": process.env.NODE_ENV == "test" ? { "node": "current" } : {},
+      "targets": { "esmodules": true },
       "loose": true
     }]
   ],
